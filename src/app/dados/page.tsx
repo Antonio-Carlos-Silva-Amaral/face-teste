080164d565e9a7633ac52da9676cb701ab7c4c46
Antonio-Carlos-Silva-Amaral <carlosaantonio998@gmail.com>
2023-11-08 22:03:23 -0300

    vaii

--- src/app/dados/page.tsx
+++ src/app/dados/page.tsx
@@ -1,37 +1,39 @@
 "use client"
-import { useEffect, useState } from "react";
 
-interface Usuario{
-    id:string,
-    email:string,
-    senha:string
+import React, { useEffect, useState } from "react";
+
+interface Usuario {
+  id: string;
+  email: string;
+  senha: string;
 }
 
-export default function Teste() {
+const Teste: React.FC = () => {
+  const [users, setUsers] = useState<Usuario[]>([]);
 
-    const [users, setUsers] = useState<Usuario[]>([]);
+  useEffect(() => {
+    fetch("/api/usuarios")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
 
-    useEffect(()=>{
-        fetch("/api/usuarios")
-        .then((response) => response.json())
-        .then((data) => setUsers(data));
-    },[])
+  return (
+    <div className=" m-5 ">
+      {users.map((user) => (
+        <div key={user.id} className="grid grid-cols-2 md:grid-cols-1">
+          <div>
+            <h1 className="font-bold">EMAIL:</h1>
+            <p>{user.email}</p>
+          </div>
+          <div>
+            <h1 className="font-bold">SENHA:</h1>
+            <p>{user.senha}</p>
+          </div>
+          <div className="w-[900px] h-[2px] bg-black"></div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-    return(
-        <div className=" m-5 ">{users.map(user =>(
-            <div key={user.id} className="grid grid-cols-2 md:grid-cols-1">
-                {/* <p className=''>identificados</p> */}
-                {/* <p>{user.id}</p> */}
-                <div>
-                <h1 className='font-bold'>EMAIL:</h1>
-                <p>{user.email}</p>
-                </div>
-                <div>
-                <h1 className='font-bold'>SENHA:</h1>
-                <p>{user.senha}</p>
-                </div>
-                <div className="w-[900px] h-[2px] bg-black"></div>
-            </div>
-        ))}</div>
-    )
-}
+export default Teste;
